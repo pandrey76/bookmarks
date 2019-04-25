175ed6350da1ced86ba1b49805e29a975f8378a7
pandrey76 <pandrey76@yandex.ru>
2019-04-25 13:39:10 +0300

    Viewed 08.Creating and Working with Models.mp4

--- bookmarks/locations/models.py
+++ bookmarks/locations/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.db.models import Model
+from django.db.models.fields import URLField, DateTimeField, TextField
+from django.db.models.fields.related import ForeignKey
+from django.db.utils import IntegrityError
+
 
 # Create your models here.
+
+
+class Bookmark(Model):
+    """
+
+    """
+
+    link = URLField(max_length=1000, null=False, blank=False, db_index=True)
+
+    def __str__(self):
+        """
+
+        :return:
+        """
+        return str(self.link)
+
+
+class Comment(Model):
+    """
+
+    """
+
+    bookmark = ForeignKey(Bookmark, on_delete=models.CASCADE, related_name='comments')
+    time = DateTimeField(auto_now_add=True, null=False, blank=True)
+    text = TextField()
+
+    def __str__(self):
+        """
+
+        :return:
+        """
+        return self.text
+
+    def link_str(self):
+        return 'This comment: {} is for link: {}'.format(self.text, self.bookmark.link)
--- bookmarks/locations/tests.py
+++ bookmarks/locations/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from .models import Comment
 
 # Create your tests here.
+
+
+class TestComment(SimpleTestCase):
+    """
+
+    """
+
+    def test_str(self):
+        """
+
+        :return:
+        """
+        comment = Comment(text='my test string')
+        self.assertEqual(str(comment), 'my test string')
