0b1f3beb4986a6b23105cd482f4fb1a546c5f640
pandrey76 <pandrey76@yandex.ru>
2020-02-12 14:05:50 +0300

    Viewed 18.Refactoring Our API with the DRF.mp4
    Ending viewing video.

--- bookmarks/locations/urls.py
+++ bookmarks/locations/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url, include
 from django.urls import path
-from locations import views
+from . import views
 from .views import SimpleHelloWord, SimpleHelloPerson, SimpleHelloPersonVersion2, TemplateHelloPerson,\
     SimpleHelloWorldAPI
+from .views import BookmarkListView, BookmarkDetailView
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('hello1/', SimpleHelloWord.as_view(), name='hello-view1'),
@@ -11,3 +13,12 @@ urlpatterns = [
     path('hello4/<str:name>/', TemplateHelloPerson.as_view(), name='hello-view4'),
     path('hello_api/<str:name>/', SimpleHelloWorldAPI.as_view(), name='hello-api'),
 ]
+
+standardview_urlpatters = [
+    url(r'^bookmarks_trade_view/$', BookmarkListView.as_view()),
+    url(r'^bookmarks_trade_view/(?P<pk>[0-9]+)/$', BookmarkDetailView.as_view())
+]
+
+standardview_urlpatters = format_suffix_patterns(standardview_urlpatters)
+
+urlpatterns += standardview_urlpatters
--- bookmarks/locations/views.py
+++ bookmarks/locations/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.views.generic import View, TemplateView
 from django.http import HttpResponse, JsonResponse
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404
+from .models import Bookmark, Comment, Note, Like
+from .serializers import BookmarkSerializer
 
 
 # Create your views here.
@@ -69,3 +74,51 @@ class SimpleHelloWorldAPI(View):
                 },
                 status=403
             )
+
+
+class BookmarkListView(APIView):
+    """
+    List all bookmarks, or create a new bookmark
+    """
+
+    def get(self, request, format=None):
+        bookmarks = Bookmark.objects.all()
+        serializer = BookmarkSerializer(bookmarks, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = BookmarkSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class BookmarkDetailView(APIView):
+    """
+    Retrive, update or delete a bookmark.
+    """
+
+    def get_object(self, pk):
+        try:
+            return Bookmark.objects.get(pk=pk)
+        except Bookmark.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        bookmark = self.get_object(pk)
+        serializer = BookmarkSerializer(bookmark)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        bookmark = self.get_object(pk)
+        serializer = BookmarkSerializer(bookmark, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        bookmark = self.get_object(pk)
+        bookmark.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
